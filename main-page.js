ba4a62a6569c8097de7d7430a172d296820870f9
Chansol Lim <lchans0319@gmail.com>
2022-01-26 18:35:46 +0900

    sub-nav 추가

--- main-page.js
+++ main-page.js
@@ -1,5 +1,7 @@
 const $criteria = document.querySelector('#search-criteria');
 const $searchBtn = document.querySelector('#search-btn');
+const $subNav = document.querySelector('#sub-nav');
+const $$navItems = document.querySelectorAll('.nav-item');
 let criteria;
 
 // 검색 기준 변경
@@ -13,3 +15,23 @@ function onClickSearchBtn() {
   return;
 }
 $searchBtn.addEventListener('click', onClickSearchBtn);
+
+// visability 변경 (클래스만으로도 바뀌도록 수정할 것)
+function activate($target) {
+  $target.classList.add('is-acitve');
+  $target.style.visibility = 'visible';
+}
+function deactivate($target) {
+  $target.classList.remove('is-acitve');
+  $target.style.visibility = 'hidden';
+}
+for (let $navItem of $$navItems) {
+  $navItem.addEventListener('mouseover', function() {
+    activate($subNav);
+    activate(this.querySelector('.nav-sub-list'));
+  });
+  $navItem.addEventListener('mouseout', function() {
+    deactivate($subNav);
+    deactivate(this.querySelector('.nav-sub-list'));
+  });
+}
